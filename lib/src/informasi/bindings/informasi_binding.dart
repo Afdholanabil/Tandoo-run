cdd31fd6264fe46d7f16c510517bf4087a397768
Afdholanabil <afdholanabil41435@gmail.com>
2023-11-08 15:25:57 +0700

    add informasi penetesan

--- lib/src/informasi/bindings/informasi_binding.dart
+++ lib/src/informasi/bindings/informasi_binding.dart
@@ -1,9 +1,14 @@
 import 'package:get/get.dart';
 import 'package:tandu_run/src/informasi/controller/informasi_controller.dart';
+import 'package:tandu_run/src/informasi_hama/controller/informasiHama_controller.dart';
+import 'package:tandu_run/src/informasi_penetesan/controller/informasiPenetesan_controller.dart';
 
 class informasiBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<informasiController>(() => informasiController());
+    Get.lazyPut<informasiPenetesanController>(
+        () => informasiPenetesanController());
+    Get.lazyPut<informasiHamaController>(() => informasiHamaController());
   }
 }
